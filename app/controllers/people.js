25d8b598d7cbe6b89e9640d1d254ca87b1ebe4a2
Kayla Jones <KaylaLyne424@gmail.com>
2014-08-01 16:12:17 -0500

    initial files

---
+++ app/controllers/people.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Person = require('../models/person');
+
+exports.index = function(req,res){
+  Person.all(function(err, people){
+
+  console.log(people);
+
+  res.render('people/index', {people:people});
+  });
+
+};
+
